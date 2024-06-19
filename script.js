0a71ff5c933dd747ef556ecc8c66ffd71b423896
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-19 15:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+	  //your JS code here.
+	let email=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+	return email.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+//const str = prompt("Enter an email address.");
+//alert(validEmail(str));
